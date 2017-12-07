cccd35ea000b81295c3a2107bcc42aa8afc1bbb5
liukai <liukai@yzyw-inc.com>
2017-12-07 14:00:08 +0800

    MOD: 增加spring boot maven 插件

--- src/main/java/com/liukai/web/SampleController.java
+++ src/main/java/com/liukai/web/SampleController.java
@@ -5,8 +5,9 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @EnableAutoConfiguration
 public class SampleController {
 
